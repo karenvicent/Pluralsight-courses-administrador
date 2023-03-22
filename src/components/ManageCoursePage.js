0b92345529122037627f11ee2d35f9929dd79239
karenvicent <karenvergara@msn.com>
2023-03-22 14:42:08 -0500

    form

---
+++ src/components/ManageCoursePage.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
+import { toast } from "react-toastify";
+
+const ManageCoursePage = (props) => {
+  const [errors, setErrors] = useState({});
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: "",
+  });
+
+  function handleChange(event) {
+    const updatedCourse = {
+      ...course,
+      [event.target.name]: event.target.value,
+    };
+    setCourse(updatedCourse);
+  }
+
+  function formIsValid() {
+    const _errors = {};
+    if (!course.title) _errors.title = "Title is required";
+    if (!course.authorId) _errors.authorId = "Author ID is required";
+    if (!course.category) _errors.category = "Category is required";
+    setErrors(_errors);
+    // form is valid if the errors object has no properties
+    return Object.keys(_errors).length === 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!formIsValid()) return;
+    courseApi.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast.success("Course saved.");
+    });
+  }
+
+  return (
+    <>
+      <h2>Manage Courses</h2>
+      <CourseForm
+        errors={errors}
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+    </>
+  );
+};
+
+export default ManageCoursePage;
